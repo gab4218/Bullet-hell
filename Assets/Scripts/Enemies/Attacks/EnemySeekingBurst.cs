626cb108cc6f8b3dd9451aab987af98265eb6919
gab4218 <gabriel.benes@davinci.edu.ar>
2025-12-07 23:55:35 -0300

    seeking attack

---
+++ Assets/Scripts/Enemies/Attacks/EnemySeekingBurst.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemySeekingBurst : IEnemyAttack
+{
+    private float _cd = 0;
+    private float _t;
+    private int _index = 0;
+    public void Attack(Transform transform, float cooldown, float speed)
+    {
+        _cd += Time.deltaTime;
+        if (_cd < cooldown) return;
+        if (_index < 4)
+        {
+            _t += Time.deltaTime;
+            if (_t < 0.5f) return;
+            var b = GameManager.instance.enemyBulletPool.GetObject();
+            _t = 0f;
+            _index++;
+            b.rotationalSpeed = 0;
+            b.speed = speed;
+            b.SetCreator(GameManager.instance.enemyBulletPool);
+            ((BulletBase)b.bullet).SetLifetime(8).SetSpeed(speed);
+            b.Shoot((Vector2)(GameManager.instance.player.transform.position - transform.position).normalized, transform.position);
+            b.Seeking();
+            return;
+        }
+        _t = 0;
+        _index = 0;
+        _cd = 0;
+    }
+}
